654119625bb0e8710483ad8dbff61ca7e528e89c
Dwayne <deandraper82@gmail.com>
2020-10-04 12:26:17 -0500

    Pushing while I work out Live Server issues and figure out how to set it up.

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// TODO
+class GroceryList extends React.Component {
+  render() {
+    return <ul>
+      <li>GroceryItem1</li>
+      <li>GroceryItem2</li>
+    </ul>
+  }
+}
+ReactDOM.render(GroceryList, document.getElementById('app'));
